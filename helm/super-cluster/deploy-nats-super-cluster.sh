5831de1aebfe4c42bfd0ec8cf8819a160876bb15
Firas GHANMI <firas.ghanmi@amadeus.com>
2023-05-16 11:12:47 +0200

    update

--- helm/super-cluster/deploy-nats-super-cluster.sh
+++ helm/super-cluster/deploy-nats-super-cluster.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
 # list of namespaces
-namespace_list=("mcpworker-production" "mcpworker-staging" "msp-production" "msp-staging")
-
-cd "/Users/amakke/workspace/NATS-Study/NATS/helm" || exit
-export PATH=$HOME/workspace/cloudTools/acsTools:$HOME/workspace/cloudTools/oc-v3.11:$PATH
+namespace_list=("nats-1" "nats-1" "nats-1")
 
 for namespace in "${namespace_list[@]}"
 do
     printf "*********************************************\n"
     printf "Redeploying on namespace: %s\n" "${namespace}"
     printf "*********************************************\n"
-    deployment="nats-${namespace}"
+    deployment="${namespace}"
     helm uninstall "${deployment}" --namespace "${namespace}"
     helm install "${deployment}" -f helm_nats_super-cluster_"${namespace}".yaml  --namespace "${namespace}" ../helm-nats/nats
     printf "***************************************************************************************************************************************\n"
